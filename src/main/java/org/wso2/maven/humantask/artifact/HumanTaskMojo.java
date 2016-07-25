208f821490bd83d224af9946b096d6f5623cc98f
drifftr <drifftr@gmail.com>
2016-07-25 18:01:42 +0530

    adding plugin sources

---
+++ src/main/java/org/wso2/maven/humantask/artifact/HumanTaskMojo.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.maven.humantask.artifact;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.wso2.maven.humantask.artifact.util.FileUtils;
+
+@Mojo(name = "buildHumanTask")
+public class HumanTaskMojo extends AbstractMojo {
+
+	@Parameter(defaultValue = "${project.basedir}")
+	private File path;
+
+	@Parameter(defaultValue = "zip")
+	private String type;
+
+	@Parameter(defaultValue = "false")
+	private boolean enableArchive;
+
+	@Parameter(defaultValue = "${project}")
+	private MavenProject mavenProject;
+
+	private static final String BPEL_CONTENT_DIR = "htcontent";
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		File project = path;
+		File bpelContentDir = new File(project, BPEL_CONTENT_DIR);
+		createZip(bpelContentDir);
+	}
+
+	public void createZip(File project) throws MojoExecutionException {
+		try {
+			String artifactType = getType();
+			String artifactName = mavenProject.getArtifactId() + "-" + mavenProject.getVersion() + "." + artifactType;
+			getLog().info("Project Name : " + project.getName());
+			getLog().info("Artifact Name : " + artifactName);
+			getLog().info("path : " + path);
+			File archive = FileUtils.createArchive(getLog(), path, project, artifactName);
+			if (archive != null && archive.exists()) {
+				mavenProject.getArtifact().setFile(archive);
+			} else {
+				throw new MojoExecutionException(archive + " is null or doesn't exist");
+			}
+		} catch (Exception e) {
+			throw new MojoExecutionException("Error while creating bpel archive", e);
+		}
+
+	}
+
+	public String getBPELProjectName(File project) {
+		List<File> fileList = FileUtils.getAllFilesPresentInFolder(project);
+		String bpelProjectName = project.getName();
+		for (File file : fileList) {
+			if (!file.isDirectory()) {
+				if (file.getName().toLowerCase().endsWith(".ht")) {
+					bpelProjectName = file.getParent();
+					return bpelProjectName;
+				}
+
+			}
+		}
+		return bpelProjectName;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setEnableArchive(boolean enableArchive) {
+		this.enableArchive = enableArchive;
+	}
+
+	public boolean isEnableArchive() {
+		return enableArchive;
+	}
+}
---
+++ src/main/java/org/wso2/maven/humantask/artifact/util/FileUtils.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.maven.humantask.artifact.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.log4j.Logger;
+import org.apache.maven.plugin.logging.Log;
+
+public class FileUtils {
+
+	private static final Logger logger = Logger.getLogger(FileUtils.class);
+
+	public static final String ERROR_CREATING_CORRESPONDING_ZIP_FILE = "Error creating corresponding ZIP file";
+
+	public static void copyDirectory(File srcPath, File dstPath, List<File> filesToBeCopied) throws IOException {
+
+		if (srcPath.isDirectory()) {
+			if (!dstPath.exists()) {
+				dstPath.mkdir();
+			}
+			String files[] = srcPath.list();
+			if (files != null) {
+				for (String file : files) {
+					copyDirectory(new File(srcPath, file), new File(dstPath, file), filesToBeCopied);
+				}
+			}
+		} else {
+			if (srcPath.exists()) {
+				org.apache.commons.io.FileUtils.copyFile(srcPath, dstPath);
+			}
+		}
+	}
+
+	public static List<File> getAllFilesPresentInFolder(File srcPath) {
+		List<File> fileList = new ArrayList<File>();
+		if (srcPath.isDirectory()) {
+			String files[] = srcPath.list();
+			if (files != null) {
+				for (String file : files) {
+					fileList.addAll(getAllFilesPresentInFolder(new File(srcPath, file)));
+				}
+			}
+		} else {
+			fileList.add(srcPath);
+		}
+		return fileList;
+	}
+
+	public static File createArchive(Log log, File location, File artifactLocation, String artifactName)
+			throws Exception {
+		List<File> allFilesPresentInFolder = getAllFilesPresentInFolder(artifactLocation);
+		File[] fileArray = new File[allFilesPresentInFolder.size()];
+		for (int i = 0; i < allFilesPresentInFolder.size(); i++) {
+			fileArray[i] = allFilesPresentInFolder.get(i);
+			log.info("FileArray name : " + allFilesPresentInFolder.get(i).getName());
+		}
+		List<File> bpelValidFileList = getFileList(fileArray);
+		if (bpelValidFileList.size() == 0) {
+			throw new Exception("The selected location " + location.getName() + "(" + location.toString()
+					+ ") does not contain any bpel processes.");
+		}
+
+		File targetFolder;
+		targetFolder = new File(location.getPath(), "target");
+		File bpelDataFolder = new File(targetFolder, "ht-tmp");
+		bpelDataFolder.mkdirs();
+		File zipFolder = new File(bpelDataFolder, artifactLocation.getName());
+		zipFolder.mkdirs();
+		copyDirectory(artifactLocation, zipFolder, bpelValidFileList);
+		log.info("Copied Size : " + getAllFilesPresentInFolder(zipFolder).size());
+		File zipFile = new File(targetFolder, artifactName);
+		zipFolder(zipFolder.getAbsolutePath(), zipFile.toString());
+		org.apache.commons.io.FileUtils.deleteDirectory(bpelDataFolder);
+		return zipFile;
+
+	}
+
+	public static List<File> getFileList(File[] fileList) {
+		List<File> list = new ArrayList<File>();
+		Collections.addAll(list, fileList);
+		return list;
+	}
+
+	public static File createTempDirectory() throws IOException {
+		File tempDirectory = createTempFile();
+		tempDirectory.delete();
+		tempDirectory.mkdirs();
+		return tempDirectory;
+	}
+
+	public static File createTempFile() throws IOException {
+		return File.createTempFile("maven", ".tmp");
+	}
+
+	static public void zipFolder(String srcFolder, String destZipFile) {
+		ZipOutputStream zip;
+		FileOutputStream fileWriter;
+		try {
+			fileWriter = new FileOutputStream(destZipFile);
+			zip = new ZipOutputStream(fileWriter);
+			addFolderContentsToZip(srcFolder, zip);
+			zip.flush();
+			zip.close();
+		} catch (IOException ex) {
+			logger.error(ERROR_CREATING_CORRESPONDING_ZIP_FILE, ex);
+		}
+	}
+
+	static private void addToZip(String path, String srcFile, ZipOutputStream zip) {
+
+		File folder = new File(srcFile);
+
+		if (folder.isDirectory()) {
+			addFolderToZip(path, srcFile, zip);
+		} else {
+			if (!srcFile.equals(".project")) {
+				byte[] buf = new byte[1024];
+				int len;
+				try {
+					FileInputStream in = new FileInputStream(srcFile);
+					String location = folder.getName();
+					if (!path.equalsIgnoreCase("")) {
+						location = path + File.separator + folder.getName();
+					}
+					zip.putNextEntry(new ZipEntry(location));
+					while ((len = in.read(buf)) > 0) {
+						zip.write(buf, 0, len);
+					}
+					in.close();
+				} catch (IOException e) {
+					logger.error(ERROR_CREATING_CORRESPONDING_ZIP_FILE, e);
+				}
+			}
+		}
+	}
+
+	static private void addFolderContentsToZip(String srcFolder, ZipOutputStream zip) {
+		File folder = new File(srcFolder);
+		String fileListe[] = folder.list();
+		int i = 0;
+		if (fileListe != null) {
+			while (i < fileListe.length) {
+				addToZip("", srcFolder + File.separator + fileListe[i], zip);
+				i++;
+			}
+		}
+	}
+
+	static private void addFolderToZip(String path, String srcFolder, ZipOutputStream zip) {
+		File folder = new File(srcFolder);
+		String fileListe[] = folder.list();
+		int i = 0;
+		while (i < fileListe.length) {
+			String newPath = folder.getName();
+			if (!path.equalsIgnoreCase("")) {
+				newPath = path + File.separator + newPath;
+			}
+			addToZip(newPath, srcFolder + File.separator + fileListe[i], zip);
+			i++;
+		}
+
+	}
+}
